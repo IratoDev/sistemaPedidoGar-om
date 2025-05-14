2f394ceed2846353779bdb87c2098eb955a1655a
IratoDev <iratodev.frontend@gmail.com>
2025-05-13 21:25:40 -0300

    correção de erro

---
+++ src/pages/dist/_documents.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var document_1 = require("next/document");
+function document() {
+    return (React.createElement(document_1.Html, null,
+        React.createElement(document_1.Head, null),
+        React.createElement("body", null,
+            React.createElement(document_1.Main, null),
+            React.createElement(document_1.NextScript, null))));
+}
+exports["default"] = document;
