208251811805054d89b2e516f5b58d61b48d69f3
IratoDev <iratodev.frontend@gmail.com>
2025-05-13 21:46:33 -0300

    demo frontend

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+import { DocumentContext, DocumentInitialProps, Document } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
