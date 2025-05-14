617618dc3a3882ebeca2b0acd64914baa7f2d668
IratoDev <iratodev.frontend@gmail.com>
2025-05-13 21:37:45 -0300

    correção de descrição de função

--- src/pages/dist/_documents.js
+++ src/pages/dist/_documents.js
@@ -1,11 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var document_1 = require("next/document");
-function document() {
-    return (React.createElement(document_1.Html, null,
-        React.createElement(document_1.Head, null),
-        React.createElement("body", null,
-            React.createElement(document_1.Main, null),
-            React.createElement(document_1.NextScript, null))));
+var react_1 = require("react");
+function Document() {
+    return (react_1["default"].createElement(document_1.Html, null,
+        react_1["default"].createElement(document_1.Head, null),
+        react_1["default"].createElement("body", null,
+            react_1["default"].createElement(document_1.Main, null),
+            react_1["default"].createElement(document_1.NextScript, null))));
 }
-exports["default"] = document;
+exports["default"] = Document;
