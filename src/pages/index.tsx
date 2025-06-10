1155503489bebae69df9e235e8b398d42f9db617
IratoDev <iratodev.frontend@gmail.com>
2025-06-10 17:39:44 -0300

    adicao do apk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,6 +77,9 @@ return(
   <h1>Usuário teste:</h1>
   <h3>Email:teste.email@gmail.com</h3>
   <p>Senha: 1234</p>
+
+  <p>Baixe o app de garçom:</p>
+  <a href="https://drive.google.com/file/d/1zvaWv97AFZ_b1suXTRGdXvj-frrmwAPV/view?usp=drive_link"/>
 </div>
 
 
